badb6bc9cb8dccfe04d39b380679b0fee02cdd21
Faith A. Dennis <32501997+FaithAB@users.noreply.github.com>
2023-11-30 14:01:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ from time import sleep
 button = Button() # did not find documentation for this 
 button.wait_for_press() # did not find documentation for this
 
-# While True: wait
-    # if button is pressed: 
-        # break
-
-# for i in range(3):
-    # camera.start_capture() <- should start capturing camera data and saving it to storage
-    # while distance < circumfrance of base:
-        # arm.start_rotate()
-    # camera.pause()
-    # servo.rotate_camera()
-    # if i == 2:
-        #camera.stop()
+# While True: 
+    # While True: wait
+        # if button is pressed: 
+            # break
     
-# Send data to cloud
-# Delete data from SSD
+    # for i in range(3):
+        # camera.start_capture() <- should start capturing camera data and saving it to storage
+        # while distance < circumfrance of base:
+            # arm.start_rotate()
+        # camera.pause()
+        # servo.rotate_camera()
+        # if i == 2:
+            #camera.stop()
+        
+    # Send data to cloud
+    # Delete data from SSD
 
