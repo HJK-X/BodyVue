337316901f57be8b5727694bfe7480220c3d8c21
Justin <34551787+HJK-X@users.noreply.github.com>
2023-12-09 11:24:16 -0800

    Update main.py

--- Datacol/main.py
+++ Datacol/main.py
@@ -1,11 +1,22 @@
-from gpiozero import Button # or manually program button 
 from time import sleep
 import RPi.GPIO as GPIO
-import time
+from adafruit_servokit import ServoKit
+from RpiMotorLib import rpi_dc_lib
+import Encoder
+import picamera
+
+def take_picture(filename,camera):
+    camera.resolution = (1024, 768)
+    camera.capture(filename)
+
+def start_video(filename, camera):
+    camera.resolution = (1024, 768)
+    camera.start_recording(filename)
+def stop_video(camera):
+    camera.stop_recording()
 
 LED_PIN = 3129301293
-BUTTON_IN_PIN = 10213
-BUTTON_OUT_PIN = 10231923
+BUTTON_PIN = 10213
 
 MOTOR_IN1_PIN = 27
 MOTOR_IN2_PIN = 22
@@ -14,26 +25,44 @@ ENCODER_CHA_PIN = 18
 ENCODER_CHB_PIN = 15
 
 
-GPIO.setup(18,GPIO.OUT)
-GPIO.output(18,GPIO.HIGH)
+GPIO.setup(LED_PIN,GPIO.OUT)
+GPIO.setup(BUTTON_PIN,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
-button = Button() # did not find documentation for this 
-button.wait_for_press() # did not find documentation for this
+motor = rpi_dc_lib.L298NMDc(MOTOR_IN1_PIN, MOTOR_IN2_PIN, MOTOR_PWM_PIN)
+enc = Encoder.Encoder(ENCODER_CHA_PIN, ENCODER_CHB_PIN)
+camera = picamera.PiCamera()
+servo = ServoKit(channels=16).servo[0]
 
-# While True: 
-    # While True: wait
-        # if button is pressed: 
-            # break
+
+angles = [100, 200, 0]
+
+while True: 
+    while True: wait
+        GPIO.output(LED_PIN,GPIO.HIGH)
+        button_state = GPIO.input(BUTTON_PIN)
+        print(button_state)
+        if button_state == 0:
+            break
+    GPIO.output(LED_PIN, GPIO.LOW)
     
-    # for i in range(3):
-        # camera.start_capture() <- should start capturing camera data and saving it to storage
-        # while distance < circumfrance of base:
-            # arm.start_rotate()
-        # camera.pause()
-        # servo.rotate_camera()
-        # if i == 2:
-            #camera.stop()
-        
-    # Send data to cloud
-    # Delete data from SSD
+    for i in range(3):
+        servo.angle = angles[i]
+        start_video("test.mp4", camera)
+
+        start = enc.read()
+        motor.forward(50)
+        while enc.read()-old < 5000:
+            a = enc.read()-old
+            sleep(.01)
+            print(a)
+            
+        print(enc.read()-old)
+        motor.brake()
+        print(enc.read()-old)
+    
+    stop_video(camera)
+
+    TOKEN = ''
+    Send data to cloud
+    Delete data from SSD
 
