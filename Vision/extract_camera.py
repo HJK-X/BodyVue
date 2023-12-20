319bc812231ed21f6cfde9f4cd0dcbbf4349e935
Justin <34551787+HJK-X@users.noreply.github.com>
2023-12-19 16:44:30 -0800

    fresh branch

--- Vision/extract_camera.py
+++
@@ -1,9 +0,0 @@
-import bpy
-import os
-
-scn = bpy.context.scene
-
-for f in range(scn.frame_start, scn.frame_end + 1, scn.frame_step):
-    scn.frame_set(f)
-    mat = scn.camera.matrix_world
-    print(mat)
